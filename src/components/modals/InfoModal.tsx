d8beadb76d2d7d7e2f7133355119f4d502df9665
sungchi <sungchi9@gmail.com>
2022-02-02 12:36:21 +0900

    Update InfoModal.tsx

--- src/components/modals/InfoModal.tsx
+++ src/components/modals/InfoModal.tsx
@@ -1,4 +1,5 @@
 import { Cell } from '../grid/Cell'
+import { Key } from '../keyboard/Key'
 import { BaseModal } from './BaseModal'
 
 type Props = {
@@ -9,35 +10,37 @@ type Props = {
 export const InfoModal = ({ isOpen, handleClose }: Props) => {
   return (
     <BaseModal title="게임방법" isOpen={isOpen} handleClose={handleClose}>
-      <p className="text-sm text-gray-500 dark:text-gray-300">
-        우리말 Wordle은 세 음절로 된 우리말 명사를 여섯번의 시도 안에 맞추는 게임입니다. 키보드에 표시되는 색은 자음, 모음을 분리해 검사합니다. (주의: 키보드에 없는 ㄳ,ㅚ 등 겹낱자는 ㄱ,ㅅ,ㅗ,ㅣ와 같이 분리해 검사)
+      <p className="p-2 text-sm text-gray-500 dark:text-gray-300 text-left">
+        세 음절로 된 우리말 명사를 입력해 맞추는 게임입니다. 6번의 기회가 주어지며 맞춰야 하는 단어에는 중복되는 자음, 모음이 없습니다. 
       </p>
+      
+      <h3 className='px-2 font-bold text-gray-500 dark:text-gray-300'>입력칸 컬러 예시</h3>
 
-      <div className="flex justify-center mb-1 mt-4">
-        <Cell value="고" status="correct" />
-        <Cell value="양" />
-        <Cell value="이" />
+      <div className="flex justify-center">
+        <Cell value="닭" status="present" />
+        <Cell value="볶" status="correct" />
+        <Cell value="음" status="absent" />
       </div>
-      <p className="text-sm text-gray-500 dark:text-gray-300">
-        '고'는 자음과 모음, 위치까지 모두 맞았습니다.
+      <p className="p-2 text-sm text-gray-500 dark:text-gray-300 text-left">
+        '볶'은 자음과 모음, 위치까지 모두 맞았습니다. '닭'은 일부 맞는 자모가 있으나 위치가 잘못되었습니다. '음'의 모든 자모는 답이 되는 단어에 존재하지 않습니다.
       </p>
-
-      <div className="flex justify-center mb-1 mt-4">
-        <Cell value="강" />
-        <Cell value="지" />
-        <Cell value="아" status="present" />
+      <h3 className='px-2 font-bold text-gray-500 dark:text-gray-300'>키보드 컬러 예시</h3>
+      <div className="flex justify-center">
+        <Key value="ㄷ" onClick={() => void 0} status="correct" />
+        <Key value="ㅏ" onClick={() => void 0} status="absent" />
+        <Key value="ㄹ" onClick={() => void 0} status="present" />
+        <Key value="ㄱ" onClick={() => void 0} status="absent" />
       </div>
-      <p className="text-sm text-gray-500 dark:text-gray-300">
-        '아'는 맞는 자모가 있으나 위치가 잘못되었습니다.
-      </p>
-
-      <div className="flex justify-center mb-1 mt-4">
-        <Cell value="화" />
-        <Cell value="분" status="absent" />
-        <Cell value="실" />
-      </div>
-      <p className="text-sm text-gray-500 dark:text-gray-300">
-        '분'의 자모는 답이 되는 단어에 존재하지 않습니다.
+      <div className="flex justify-center mt-1">
+      <Key value="ㅂ" onClick={() => void 0} status="correct" />
+        <Key value="ㅗ" onClick={() => void 0} status="correct" />
+        <Key value="ㄲ" onClick={() => void 0} status="correct" />
+        <Key value="ㅇ" onClick={() => void 0} status="absent" />
+        <Key value="ㅡ" onClick={() => void 0} status="absent" />
+        <Key value="ㅁ" onClick={() => void 0} status="absent" />
+      </div> 
+      <p className="p-2 text-sm text-gray-500 dark:text-gray-300 text-left">
+        키보드에 표시되는 색은 자음, 모음을 분리해 검사합니다. 쌍자음과 달리 키보드에 없는 ㄳ,ㅚ 등 겹낱자는 ㄱ,ㅅ,ㅗ,ㅣ와 같이 분리해 검사합니다.
       </p>
     </BaseModal>
   )
